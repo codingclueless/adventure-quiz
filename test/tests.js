671d8aa19938b1399b43bded41b98014ed5954b5
Wes Griffin <themisguidedidiot@gmail.com>
2019-05-17 09:03:29 -0700

    cant remember what i changed

--- test/tests.js
+++ test/tests.js
@@ -5,4 +5,4 @@ import './create-quest-link.test.js';
 import './create-choice.test.js';
 import './score.quest.test.js';
 import './has-completed-all-quests.test.js';
-// import './is-dead.test.js';
+import './is-dead.test.js';
